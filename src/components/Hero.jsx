b32b352ef7cc5f552905045b5b794fdc7180d081
Christopher Yuen <chriskaijoyu@gmail.com>
2024-09-21 20:33:24 -0400

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
+import PublicDebate from './PublicDebate';
+
 const Hero = () => {
   return (
     <div className="flex-1 bg-slate-50 p-16 overflow-auto ml-[180px]">
       <div className="max-h-[500px]">
         <h1 className="text-3xl text-gray-800">Placeholder placeholder placeholder placeholder</h1>
         <p className="mt-2 tracking-wide text-xl leading-7 text-gray-700">
-        placeholderplaceholderplaceholderplaceholderplaceholderplaceholderplaceholder
+          placeholderplaceholderplaceholderplaceholderplaceholderplaceholderplaceholder
         </p>
+        <PublicDebate />
       </div>
     </div>
   );
